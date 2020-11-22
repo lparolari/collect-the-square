5a315818fdc7a26d92b47b1bf751c0632d1ee6d0
lparolari <luca.parolari23@gmail.com>
2020-11-22 21:03:31 +0100

    chore: cleanup

--- src/__tests__/greetings.test.ts
+++
@@ -1,7 +0,0 @@
-import { greet } from "../greetings";
-
-describe("greet", () => {
-  test('it returns "Hello ${name}!" for greeting name', () => {
-    expect(greet("John")).toBe("Hello John!");
-  });
-});
